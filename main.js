c051daeb8d41b2ab65caa599b59552d16f7918af
tubi55 <tubi55@nate.com>
2022-11-28 11:55:14 +0900

    초기 설정

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ const boxs = frame.querySelectorAll('article');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
+frame.prepend(frame.lastElementChild)
+
 next.addEventListener('click', () => frame.append(frame.firstElementChild));
 prev.addEventListener('click', () => frame.prepend(frame.lastElementChild));
