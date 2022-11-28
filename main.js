d426017d5e0d1b9ef4a43f8f2a3ff5c24067f8d4
tubi55 <tubi55@nate.com>
2022-11-28 12:08:03 +0900

    모션중 재이벤트 방지

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const frame = document.querySelector('section');
-const boxs = frame.querySelectorAll('article');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+let enableClick = true;
+const delay = convertSpeed(frame.children[0]);
 
-frame.prepend(frame.lastElementChild)
+frame.prepend(frame.lastElementChild);
 
-next.addEventListener('click', () => frame.append(frame.firstElementChild));
-prev.addEventListener('click', () => frame.prepend(frame.lastElementChild));
+next.addEventListener('click', () => {
+  if (!enableClick) return;
+  frame.append(frame.firstElementChild);
+  enableClick = false;
+  setTimeout(() => enableClick = true, delay)
+
+});
+prev.addEventListener('click', () => {
+  if (!enableClick) return;
+  frame.prepend(frame.lastElementChild);
+  enableClick = false;
+  setTimeout(() => enableClick = true, delay)
+});
+
+function convertSpeed(el) {
+  return parseFloat(getComputedStyle(el).transitionDuration) * 1000;
+}
+
+
+
